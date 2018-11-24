ed7fb31e2402a995c23912db3abf59c2d0800421
Jaakko Paju <JPaju@users.noreply.github.com>
2018-11-25 01:59:35 +0200

    Added GET /api/persons

--- index.js
+++ index.js
@@ -5,65 +5,34 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 
 
-let notes = [
+let contacts = [
   {
-    id: 1,
-    content: 'HTML on helppoa',
-    date: '2017-12-10T17:30:31.098Z',
-    important: true
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: 1
   },
   {
-    id: 2,
-    content: 'Selain pystyy suorittamaan vain javascriptiä',
-    date: '2017-12-10T18:39:34.091Z',
-    important: false
+    name: "Martti Tienari",
+    number: "040-123456",
+    id: 2
   },
   {
-    id: 3,
-    content: 'HTTP-protokollan tärkeimmät metodit ovat GET ja POST',
-    date: '2017-12-10T19:20:14.298Z',
-    important: true
+    name: "Arto Järvinen",
+    number: "040-123456",
+    id: 3
+  },
+  {
+    name: "Lea Kutvonen",
+    number: "040-123456",
+    id: 4
   }
 ]
 
 
 
-app.get('/notes', (req, res) => {
-  res.json(notes)
+app.get('/api/persons', (request, response) => {
+  response.json(contacts)
 })
-
-app.get('/notes/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const note = notes.find(n => n.id === id)
-  note ?
-    res.json(note) :
-    res.status(404).end()
-})
-
-app.post('/notes', (request, response) => {
-  const body = request.body
-  if (!body.content)
-    return response.status(400).json({ error: 'content is required' })
-
-  const note = {
-    id: generateId(),
-    content: body.content,
-    date: new Date(),
-    important: body.important || false
-  }
-
-  notes = notes.concat(note)
-  response.json(body)
-})
-
-const generateId = () => notes.length > 0 ? notes.map(n => n.id).sort((a, b) => a - b).reverse()[0] + 1 : 1
-
-app.delete('/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-  response.status(204).end()
-})
-
 
 
 const PORT = 3001

53da206e024b9521d90c0203d45f548d79ff554e
Jaakko Paju <JPaju@users.noreply.github.com>
2018-11-25 01:47:53 +0200

    Added POST&DELETE

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+
 
 let notes = [
   {
@@ -22,15 +26,45 @@ let notes = [
   }
 ]
 
+
+
 app.get('/notes', (req, res) => {
   res.json(notes)
 })
 
 app.get('/notes/:id', (req, res) => {
-  const id = request.params.id
-  const note = notes.find(note => note.id === id)
-  response.json(note)
+  const id = Number(req.params.id)
+  const note = notes.find(n => n.id === id)
+  note ?
+    res.json(note) :
+    res.status(404).end()
 })
+
+app.post('/notes', (request, response) => {
+  const body = request.body
+  if (!body.content)
+    return response.status(400).json({ error: 'content is required' })
+
+  const note = {
+    id: generateId(),
+    content: body.content,
+    date: new Date(),
+    important: body.important || false
+  }
+
+  notes = notes.concat(note)
+  response.json(body)
+})
+
+const generateId = () => notes.length > 0 ? notes.map(n => n.id).sort((a, b) => a - b).reverse()[0] + 1 : 1
+
+app.delete('/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+  response.status(204).end()
+})
+
+
 
 const PORT = 3001
 app.listen(PORT, () => {

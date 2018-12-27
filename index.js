ba54b2d5cae93ef36870bbe341d575989f54502b
Jaakko Paju <JPaju@users.noreply.github.com>
2018-12-27 18:26:51 +0200

    Added comments

--- index.js
+++ index.js
@@ -2,32 +2,14 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const morgan = require('morgan')
 
 app.use(bodyParser.json())
 app.use(cors())
 app.use(express.static('build'))
 
-let notes = [
-  {
-    id: 1,
-    content: 'HTML on helppoa',
-    date: '2017-12-10T17:30:31.098Z',
-    important: true
-  },
-  {
-    id: 2,
-    content: 'Selain pystyy suorittamaan vain javascriptiä',
-    date: '2017-12-10T18:39:34.091Z',
-    important: false
-  },
-  {
-    id: 3,
-    content: 'HTTP-protokollan tärkeimmät metodit ovat GET ja POST',
-    date: '2017-12-10T19:20:14.298Z',
-    important: true
-  }
-]
-
+morgan.token('json',(req, res) => JSON.stringify(req['body']))
+app.use(morgan(':method :url :json :status :res[content-length] - :response-time ms'))
 
 
 app.get('/api/notes', (req, res) => {
@@ -52,6 +34,25 @@ app.post('/api/notes', (request, response) => {
     content: body.content,
     date: new Date(),
     important: body.important || false
+<<<<<<< HEAD
+=======
+  })
+
+  note
+    .save()
+    .then(savedNote => {
+      response.json(formatNote(savedNote))
+    })
+})
+
+
+app.put('/api/notes/:id', (request, response) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important
+>>>>>>> 8fc6c26... Added comments
   }
 
   notes = notes.concat(note)
@@ -64,9 +65,15 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end()
 })
 
-const generateId = () => notes.length > 0 ? notes.map(n => n.id).sort((a, b) => a - b).reverse()[0] + 1 : 1
+//Formats note to from database format to frontend
+const formatNote = (note) => {
+  const formattedNote = { ...note._doc, id: note._id }
+  delete formattedNote._id
+  delete formattedNote.__v
+  return formattedNote
+}
 
-
+//Start application on Heroku-binded port or port 3001
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)

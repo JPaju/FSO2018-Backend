edf7bda134282c8ff47fc4cd05a357b3241dee18
Jaakko Paju <JPaju@users.noreply.github.com>
2018-12-27 20:36:49 +0200

    Modified MongoDB-connection

--- modules/note.js
+++ modules/note.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 
-const url = process.env.MONGODB_URL
+const url = process.env.MONGODB_NOTES_URL
 mongoose.connect(url, {useNewUrlParser: true})
 
 const Note = mongoose.model('Note', {

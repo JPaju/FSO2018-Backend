35bea03ef148aeedef2b0be0ce34d0a8d8d9f41d
Jaakko Paju <JPaju@users.noreply.github.com>
2018-12-27 23:26:09 +0200

    Heroku deployment commit

---
+++ build/precache-manifest.0089b5de5c97d778e1a1891d95efddeb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a53a76cab0496df258c0",
+    "url": "/static/css/main.5228c7bd.chunk.css"
+  },
+  {
+    "revision": "a53a76cab0496df258c0",
+    "url": "/static/js/main.a53a76ca.chunk.js"
+  },
+  {
+    "revision": "800b39987ccbe34f3835",
+    "url": "/static/js/1.800b3998.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "95ae1114f98ffdb2352c3d6da2f92a9f",
+    "url": "/index.html"
+  }
+];

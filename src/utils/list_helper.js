efaa80c09a8fab917549245ede54c75b01221a48
Jaakko Paju <JPaju@users.noreply.github.com>
2018-12-29 20:12:29 +0200

    Added favoritBlog and tests for it

--- src/utils/list_helper.js
+++ src/utils/list_helper.js
@@ -1,11 +1,18 @@
-const dummy = (blogs) => 1
+const dummy = () => 1
 
 
 const totalLikes = (blogs) => (
     blogs.reduce((sum, blog) => (sum + blog.likes), 0)
 )
 
+const favoriteBlog = (blogs) => (
+    blogs.reduce((favorite, blog) => (
+        favorite.likes > blog.likes ? favorite : blog
+    ), blogs.length > 0 ? blogs[0] : undefined)
+)
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }

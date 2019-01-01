2bb03b835d5cf2d44f08f62a1b3e83ebc23bf321
Jaakko Paju <JPaju@users.noreply.github.com>
2019-01-01 19:25:57 +0200

    Added test for POST blog

--- src/controllers/blogs.js
+++ src/controllers/blogs.js
@@ -10,6 +10,13 @@ blogsRouter.get('/', (request, response) => {
 })
 
 blogsRouter.post('/', (request, response) => {
+
+    const body = request.body
+
+    if (!body.title || !body.author || !body.url || !body.likes) {
+        response.status(400).json({ error: 'Title, author, url and likes are required' })
+    }
+
     const blog = new Blog(request.body)
 
     blog
--- src/tests/blogs_api.test.js
+++ src/tests/blogs_api.test.js
@@ -1,18 +1,54 @@
 const supertest = require('supertest')
 const { app, server } = require('../index')
 const api = supertest(app)
+const Blog = require('../models/blog')
+const { initialBlogs, testBlog, blogsInDb, format, nonExistingId } = require('./test_helper')
 
-describe('GET blogs', () => {
+describe('When there is content in the DB', async () => {
 
-    test('blogs are returned', async () => {
+    beforeAll(async () => {
+        await Blog.remove({})
 
-        await api
-            .get('/api/blogs')
-            .expect(200)
-            .expect('Content-Type', /application\/json/)
+        const blogs = initialBlogs.map(b => new Blog(b))
+        await Promise.all(blogs.map(b => b.save()))
+    })
 
+
+    describe('GET blogs', () => {
+
+        test('blogs are returned', async () => {
+
+            await api
+                .get('/api/blogs')
+                .expect(200)
+                .expect('Content-Type', /application\/json/)
+
+            const returnedBlogs = await blogsInDb()
+
+            expect(returnedBlogs).toContainEqual(initialBlogs[0])
+        })
+    })
+
+
+    describe('POST blogs', () => {
+
+        test('blog can be added', async () => {
+
+            const postResponse = await api
+                .post('/api/blogs')
+                .send(testBlog)
+                .expect(201)
+                .expect('Content-Type', /application\/json/)
+
+            const returnedBlog = postResponse.body
+            delete returnedBlog.id
+
+            const blogsAfterPost = await blogsInDb()
+
+            expect(returnedBlog).toEqual(testBlog)
+            expect(blogsAfterPost.length).toBe(initialBlogs.length + 1)
+        })
     })
 })
-
 
 afterAll(() => server.close())
